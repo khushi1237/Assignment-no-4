627e87a5d35393223935f997de2222d8bea8856f
khushi1237 <96117174+khushi1237@users.noreply.github.com>
2022-01-19 16:02:03 +0500

    Add files via upload

---
+++ khushiAssiment4.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+void main(){
+  //Q no:1
+  
+  //            Write a program to print multiples of 5 ranging 1 to 100.
+  //            List Multiples_of_5 =[];
+  // for(var i=0;i<=100;i++){
+  //       if(i%5==0){
+  //        Multiples_of_5.add(i);
+  // }
+  // else{
+  // }
+  // }
+  // print("multiple of 5 are:5:$Multiples_of_5");
+
+//Q no:2
+
+//     k=[24,53,78,91,12]
+
+//   List k1=[24,53,78,91,12];
+//     num small=s1[0];
+//   for(var i in k1){
+//     if(small>i){
+//       small=i;
+//     }
+//   }
+//   print(small);
+
+//Q n0 :3 
+
+// H=[24,53,78,91,12]
+
+//  List H2=[24,53,78,91,12];
+//       num large=s1[0];
+//   for(var i in s1){
+//       if(large<i){
+//       large=i;
+//     }
+//   }
+//       print(large);
+
+
+Q no :4
+ 
+//   stdout.write("Enter table number =");
+//   int g1=int.parse(stdin.readLineSync()!);
+
+//   stdout.write("How many times you want to multiply =");
+//   int h1=int.parse(stdin.readLineSync()!);
+
+//   for(var i=1;i<=n1;i++){
+//     print(" $g1 *$i= ${h1*i} ");
+//    }
+
+// Q no :5
+
+//   List a=[24,53,78,91,12];
+  
+//   // for(var i=0 ;i<s2.length;i++){
+//   //   if(s2[i])
+//   //  if(i>s2[i]){
+//   //    var temp 
+//   //  }
+//   // }
+//   // print(s2);
+// for(var i=0;i<a.length;i++){
+//     for(var j=0;j<i;j++){
+//       if(a[i]<a[j]){
+//         var temp=a[i];
+//         a[i]=a[j];
+//         a[j]=temp;
+
+//       }
+//     }
+   
+//   }
+//   print(a);
+// Q  No :6
+// a)
+
+// for(var i=1;i<=15;i++){
+//   print(i);
+// }
+
+// b)
+// for(var i=10;i>=1;i--){
+  
+//   print(i);
+// }
+
+// c)
+
+// for(var i=0;i<=20;i++){
+//   if(i%2==0){
+//     print(i);
+//   }
+// }
+// d)
+
+// for(var i=1;i<20;i++){
+//   if(i%2!=0){
+//     print(i);
+//   }
+// }
+
+// e)
+  
+//   for(var i=2;i<=20;i++){
+//   if(i%2==0){
+//     print("${i}k");
+//   }
+// }
+//   }
